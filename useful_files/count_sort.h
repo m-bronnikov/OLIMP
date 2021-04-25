cbd10d46e536e3e06d3883813a519e2497a57309
Maksim Bronnikov <max120199@gmail.com>
2021-04-25 16:09:30 +0300

    updated count sort

--- useful_files/count_sort.h
+++ useful_files/count_sort.h
@@ -8,30 +8,35 @@
 
 #define SUPR 1000000 // Max number
 
-unsigned* Count_Sort(unsigned* A, unsigned buzy);
+unsigned* Count_Sort(unsigned* A, unsigned size);
 
 
 // count sort
-unsigned* Count_Sort(unsigned* A, unsigned buzy){
-  if(!buzy){
+unsigned* Count_Sort(unsigned* A, unsigned size){
+  if(!size){
     return A;
   }
-  unsigned j = 0;
+
+  unsigned* B = (unsigned*) malloc(sizeof(unsigned) * size);
   unsigned* C = (unsigned*)malloc(sizeof(unsigned) * SUPR);
   memset(C, 0, SUPR*sizeof(unsigned));
-  for(j = 0; j < buzy; j++){
+
+  // hystogram
+  for(unsigned j = 0; j < size; j++){
     ++C[A[j]];
   }
-  for(j = 1; j < SUPR; ++j){
+
+  // inclusive scan
+  for(unsigned j = 1; j < SUPR; ++j){
     C[j]+=C[j-1];
   }
-  unsigned* B = (unsigned*) malloc(sizeof(unsigned) * buzy);
-  for(j=buzy-1; j>0; --j){
+
+  // sorting
+  for(int j= size - 1; j >= 0; --j){
     --C[A[j]];
-    B[C[A[j]]] = A[j]; // in ~half of itterations
+    B[C[A[j]]] = A[j];
   }
-  --C[A[0]];
-  B[C[A[0]]] = A[0];
+
   free(A);
   free(C);
   return B;
